4b305e085884ecb4638a843bd60fbfec737b3bb5
Daniel <daniel.ordonez@wolox.co>
2021-06-24 16:04:56 -0500

    avanced library screen

---
+++ wbooks/src/app/screens/Libarary/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { SafeAreaView } from 'react-native';
+/* import Book from '@components/Book';
+import { BOOKS_MOCK } from '@constants/mockBooks';*/
+
+function Library() {
+  return (
+    <SafeAreaView>
+      {/* <FlatList data={BOOKS_MOCK} renderItem={Book} keyExtractor={item => item.id} />*/}
+    </SafeAreaView>
+  );
+}
