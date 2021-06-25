050cf66a401aa666167ef8d10dc56cf01a65681b
Daniel <daniel.ordonez@wolox.co>
2021-06-25 01:01:48 -0500

    change name prop

--- wbooks/src/app/components/Book/index.tsx
+++ wbooks/src/app/components/Book/index.tsx
@@ -4,7 +4,7 @@ import { Text, View, Image } from 'react-native';
 import styles from './styles';
 
 interface Props {
-  book: Book;
+  item: Book;
 }
 interface Book {
   id: Number;
@@ -16,17 +16,17 @@ interface Book {
   imageUrl: String | null;
 }
 
-function Book({ book }: Props) {
+function Book({ item }: Props) {
   return (
     <View style={styles.container}>
       <Image
         style={styles.imagen}
-        source={book.imageUrl ? { uri: book.imageUrl } : require('@assets/img_book_placeholder.png')}
+        source={item.imageUrl ? { uri: item.imageUrl } : require('@assets/img_book_placeholder.png')}
         resizeMode="contain"
       />
       <View style={styles.containerText}>
-        <Text style={styles.title}>{book.title}</Text>
-        <Text style={styles.author}>{book.author}</Text>
+        <Text style={styles.title}>{item.title}</Text>
+        <Text style={styles.author}>{item.author}</Text>
       </View>
     </View>
   );
