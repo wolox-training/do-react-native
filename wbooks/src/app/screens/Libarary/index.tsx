e1005bd0fe14ca68c17903a71db1e6d1535e615b
Daniel <daniel.ordonez@wolox.co>
2021-06-25 01:00:57 -0500

    flatlist

--- wbooks/src/app/screens/Libarary/index.tsx
+++ wbooks/src/app/screens/Libarary/index.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import { SafeAreaView } from 'react-native';
-/* import Book from '@components/Book';
-import { BOOKS_MOCK } from '@constants/mockBooks';*/
+import { FlatList, SafeAreaView } from 'react-native';
+import Book from '@components/Book';
+import { BOOKS_MOCK } from '@constants/mockBooks';
 
 export function Library() {
   return (
     <SafeAreaView>
-      {/* <FlatList data={BOOKS_MOCK} renderItem={Book} keyExtractor={item => item.id} />*/}
+      <FlatList data={BOOKS_MOCK} renderItem={Book} keyExtractor={item => item.id.toString()} />
     </SafeAreaView>
   );
 }
