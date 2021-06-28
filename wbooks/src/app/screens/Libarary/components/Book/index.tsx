777ad5cb9e690870336d02010e066c00deb4f0ad
Daniel <daniel.ordonez@wolox.co>
2021-06-28 15:58:28 -0500

    navaigation in Book component

--- wbooks/src/app/screens/Libarary/components/Book/index.tsx
+++ wbooks/src/app/screens/Libarary/components/Book/index.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
-import { Text, View, Image } from 'react-native';
+import { Text, View, Image, TouchableNativeFeedback } from 'react-native';
 import placeholder from '@assets/img_book_placeholder.png';
 import { Book as BookInterfaces } from '@interfaces/index';
+import { useNavigation } from '@react-navigation/native';
+import Routes from '@constants/routes';
 
 import styles from './styles';
 
@@ -11,19 +13,25 @@ interface Props {
 
 function Book({ book }: Props) {
   const { imageUrl, title, author } = book;
+  const navigation = useNavigation();
   const titleWithoutSpaces = title.replace(/\s/, '');
+  function handleOpenBookDetail() {
+    navigation.navigate(Routes.bookDetailListRoute, { book });
+  }
   return (
-    <View style={styles.container}>
-      <Image
-        style={styles.imagen}
-        source={imageUrl ? { uri: imageUrl as string } : placeholder}
-        resizeMode="contain"
-      />
-      <View style={styles.containerText}>
-        <Text style={styles.title}>{titleWithoutSpaces.trim()}</Text>
-        <Text style={styles.author}>{author}</Text>
+    <TouchableNativeFeedback onPress={handleOpenBookDetail}>
+      <View style={styles.container}>
+        <Image
+          style={styles.imagen}
+          source={imageUrl ? { uri: imageUrl as string } : placeholder}
+          resizeMode="contain"
+        />
+        <View style={styles.containerText}>
+          <Text style={styles.title}>{titleWithoutSpaces.trim()}</Text>
+          <Text style={styles.author}>{author}</Text>
+        </View>
       </View>
-    </View>
+    </TouchableNativeFeedback>
   );
 }
 export default Book;
