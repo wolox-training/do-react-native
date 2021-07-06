63d4727610430427c8093d268b7377e55d1cbd0c
Daniel <daniel.ordonez@wolox.co>
2021-07-06 12:11:50 -0500

    StatusBar

--- wbooks/src/app/index.tsx
+++ wbooks/src/app/index.tsx
@@ -9,12 +9,14 @@
  */
 
 import React from 'react';
+import { StatusBar } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import AppNavigator from '@components/AppNavigator';
 
 const App = () => {
   return (
     <NavigationContainer>
+      <StatusBar translucent />
       <AppNavigator />
     </NavigationContainer>
   );
