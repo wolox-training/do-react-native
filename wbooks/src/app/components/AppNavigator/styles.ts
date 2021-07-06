7567ca0c03a5fd32db856589741547a5aff4b5b7
Daniel <daniel.ordonez@wolox.co>
2021-07-06 15:44:21 -0500

    backgroundColor in headerbar style

--- wbooks/src/app/components/AppNavigator/styles.ts
+++ wbooks/src/app/components/AppNavigator/styles.ts
@@ -1,9 +1,11 @@
 import { StyleSheet } from 'react-native';
+import COLORS from '@constants/colors';
 
 export default StyleSheet.create({
   headerBar: {
     width: '100%',
-    flex: 1
+    flex: 1,
+    backgroundColor: COLORS.polar
   },
   headerIcons: {
     marginHorizontal: 13
