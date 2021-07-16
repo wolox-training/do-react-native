3410ccd67f70d910eeb6bfc2f0a120b2837da7ea
Daniel <daniel.ordonez@wolox.co>
2021-07-16 15:21:33 -0500

    store n install plugin reactotron

--- wbooks/src/config/ReactotronConfig.js
+++ wbooks/src/config/ReactotronConfig.js
@@ -1,4 +1,10 @@
 import Reactotron from 'reactotron-react-native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import { reactotronRedux } from 'reactotron-redux';
 
-Reactotron.configure({ name: 'wbooks' }).setAsyncStorageHandler(AsyncStorage).useReactNative().connect();
+const reactotron = Reactotron.configure({ name: 'wbooks' })
+  .setAsyncStorageHandler(AsyncStorage)
+  .useReactNative()
+  .use(reactotronRedux())
+  .connect();
+export default reactotron;
