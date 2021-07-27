08853e93a18da0d48a92c9bde554759fd36e6f02
Daniel <daniel.ordonez@wolox.co>
2021-07-27 15:03:33 -0500

    install thunk, implementation  in store

--- wbooks/src/redux/store.ts
+++ wbooks/src/redux/store.ts
@@ -1,8 +1,9 @@
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, compose, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
 import Reactotron from '@config/reactotronConfig';
 
 import book from './book/reducer';
 
 export const rootReducer = combineReducers({ book });
-
-export default createStore(rootReducer, Reactotron.createEnhancer!());
+const middleware = applyMiddleware(thunk);
+export default createStore(rootReducer, compose(middleware, Reactotron.createEnhancer!()));
