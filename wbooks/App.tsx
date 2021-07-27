93d182eb1cb4ecf0ec6044b1f2bec2f1210f12eb
Daniel <daniel.ordonez@wolox.co>
2021-07-27 14:09:01 -0500

    change import store

--- wbooks/App.tsx
+++ wbooks/App.tsx
@@ -4,7 +4,7 @@ import React from 'react';
 import { Provider } from 'react-redux';
 
 import App from './src/app';
-import { store } from './src/redux/store';
+import store from './src/redux/store';
 
 export default function index() {
   return (
