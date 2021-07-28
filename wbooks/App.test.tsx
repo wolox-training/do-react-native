9779c640463076ba7fcaa059eaf25eba8d3cfafd
Daniel <daniel.ordonez@wolox.co>
2021-07-28 15:00:41 -0500

    ci fixed

--- wbooks/App.test.tsx
+++ wbooks/App.test.tsx
@@ -8,12 +8,11 @@ import renderer from 'react-test-renderer';
 import configureStore from 'redux-mock-store';
 import { Provider } from 'react-redux';
 
-import { initialState as bookInitialState } from './src/redux/book/reducer';
 import App from './src/app';
 
 const middlewares = [];
 const mockStore = configureStore(middlewares);
-const initialState = { book: bookInitialState };
+const initialState = { book: { books: [] } };
 const store = mockStore(initialState);
 // Note: test renderer must be required after react-native.
 it('renders correctly', () => {
