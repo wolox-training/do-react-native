36f4f0a79a0e917838273b255f44f9dfdaa4d430
Daniel <daniel.ordonez@wolox.co>
2021-07-28 15:39:08 -0500

    middleware thunks

--- wbooks/App.test.tsx
+++ wbooks/App.test.tsx
@@ -6,11 +6,12 @@ import 'react-native';
 import React from 'react';
 import renderer from 'react-test-renderer';
 import configureStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
 import { Provider } from 'react-redux';
 
 import App from './src/app';
 
-const middlewares = [];
+const middlewares = [thunk];
 const mockStore = configureStore(middlewares);
 const initialState = { book: { books: [] } };
 const store = mockStore(initialState);
