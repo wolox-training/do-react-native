daeb16d9ec213d9b64dc31c19d635b575d39bb32
Daniel <daniel.ordonez@wolox.co>
2021-07-29 17:04:07 -0500

    add  'reactotron-react-native' in index.d.ts

--- wbooks/src/config/api.ts
+++ wbooks/src/config/api.ts
@@ -1,11 +1,13 @@
 import { create } from 'apisauce';
 import Config from 'react-native-config';
-// import Reactotron from '@config/reactotronConfig';
+import Reactotron from '@config/reactotronConfig';
 
 const api = create({
   baseURL: Config.API_BASE_URL,
   timeout: 5000
 });
-// api.addMonitor(Reactotron.apisauce);
+if (__DEV__) {
+  api.addMonitor(Reactotron.apisauce);
+}
 
 export default api;
