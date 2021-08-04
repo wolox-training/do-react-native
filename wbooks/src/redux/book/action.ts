7054653c973d9c4a35ad899f4ca2e90feab1a02f
dezbrid <dezbrid@gmail.com>
2021-08-04 11:31:59 -0500

    RR

--- wbooks/src/redux/book/action.ts
+++ wbooks/src/redux/book/action.ts
@@ -1,13 +1,15 @@
-import { Dispatch } from 'redux';
+// import { Dispatch } from 'redux';
 import BookService from '@services/BookService';
+import { createTypes } from 'redux-recompose';
 
+export const actions = createTypes(['GET_BOOKS'], '@@BOOK');
 export enum ActionsType {
   GET_BOOKS = 'GET_BOOKS',
   GET_BOOKS_SUCCESS = 'GET_BOOKS_SUCCESS',
   GET_BOOKS_FAILURE = 'GET_BOOKS_FAILURE'
 }
 
-const actionCreators = {
+/* const actionCreators = {
   getBooks: () => async (dispatch: Dispatch) => {
     dispatch({ type: ActionsType.GET_BOOKS });
     const response = await BookService.getBooks();
@@ -17,6 +19,14 @@ const actionCreators = {
       dispatch({ type: ActionsType.GET_BOOKS_FAILURE, payload: response.problem });
     }
   }
+};*/
+
+const actionCreators = {
+  getBooks: () => ({
+    type: actions.GET_BOOKS,
+    target: 'books',
+    service: BookService.getBooks
+  })
 };
 
 export default actionCreators;
--- wbooks/src/redux/store.ts
+++ wbooks/src/redux/store.ts
@@ -1,11 +1,13 @@
-import { createStore, combineReducers, compose, applyMiddleware } from 'redux';
+import { createStore, combineReducers as CR, compose, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import Reactotron from '@config/reactotronConfig';
+import { fetchMiddleware, wrapCombineReducers } from 'redux-recompose';
 
 import book from './book/reducer';
 
+const combineReducers = wrapCombineReducers(CR);
 export const rootReducer = combineReducers({ book });
-const middlewares = [applyMiddleware(thunk)];
+const middlewares = [applyMiddleware(thunk), applyMiddleware(fetchMiddleware)];
 if (__DEV__) {
   middlewares.push(Reactotron.createEnhancer());
 }
