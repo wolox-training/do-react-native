6feb9c662337c54237643fc4c7e8c6a4fdf47b61
dezbrid <dezbrid@gmail.com>
2021-08-12 11:56:48 -0500

    useMemo

--- wbooks/src/app/screens/Search/index.tsx
+++ wbooks/src/app/screens/Search/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import { useSelector } from 'react-redux';
 import { State } from '@interfaces/state';
 import { Book as BookInterface } from '@interfaces/book';
@@ -9,9 +9,13 @@ import EmptyFilter from './components/EmptyFilter';
 function Search() {
   const filterSearch = useSelector<State, string>(state => state.book.filterSearch).trim();
   const books = useSelector<State, BookInterface[]>(state => state.book.books);
-  const booksFilter = books.filter((book: BookInterface) => {
-    return book.title.toLowerCase().includes(filterSearch.toLowerCase());
-  });
+  const booksFilter = useMemo(
+    () =>
+      books.filter((book: BookInterface) => {
+        return book.title.toLowerCase().includes(filterSearch.toLowerCase());
+      }),
+    [filterSearch, books]
+  );
   const filterEmpty = filterSearch === '';
   return (
     <ListBooks
