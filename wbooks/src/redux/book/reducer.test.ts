30a2ff75a21ebb6ffbf22d5fff75fc219a89c7e2
dezbrid <dezbrid@gmail.com>
2021-08-19 16:13:06 -0500

    GET_BOOKS_SUCCESS

--- wbooks/src/redux/book/reducer.test.ts
+++ wbooks/src/redux/book/reducer.test.ts
@@ -1,3 +1,5 @@
+import { BOOKS_MOCK } from '@constants/mockBooks';
+
 import { actions } from './action';
 import booksReducer from './reducer';
 
@@ -5,7 +7,17 @@ const testActions = {
   getBooks: {
     type: actions.GET_BOOKS,
     target: 'books'
+  },
+  getBooksSuccess: {
+    type: actions.GET_BOOKS_SUCCESS,
+    target: 'books',
+    payload: BOOKS_MOCK
   }
+  /* getBooksFailure: {
+    type: actions.GET_BOOKS_FAILURE,
+    target: 'books',
+    payload: BOOKS_MOCK
+  }*/
 };
 
 describe('@@BOOKS/GET_BOOKS', () => {
@@ -14,6 +26,13 @@ describe('@@BOOKS/GET_BOOKS', () => {
       books: [],
       booksError: null,
       booksLoading: true
+    });
+  });
+  test('GET_BOOKS_SUCCESS', () => {
+    expect(booksReducer(undefined, testActions.getBooksSuccess)).toMatchObject({
+      books: BOOKS_MOCK,
+      booksError: null,
+      booksLoading: false
     });
   });
 });
