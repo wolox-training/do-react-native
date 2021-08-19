ae447f55fad0b8e6e87325c0bb9a5fb297034f4f
dezbrid <dezbrid@gmail.com>
2021-08-19 14:34:53 -0500

    fixed pr

--- wbooks/src/utils/stringUtils.test.ts
+++ wbooks/src/utils/stringUtils.test.ts
@@ -22,11 +22,17 @@ describe('keep only number', () => {
   it('get 2021 ', () => {
     expect(keepNumbers('wolox 2021')).toEqual('2021');
   });
-  it('get 2021 ', () => {
+  it('get 192021 ', () => {
     expect(keepNumbers('19 de agosto 2021')).toEqual('192021');
   });
-  it('2021', () => {
+  it('read number 2021  return string ', () => {
     expect(keepNumbers(2021)).toEqual('2021');
+  });
+  it('read number 100,25 , return 10025', () => {
+    expect(keepNumbers(100.25)).toEqual('10025');
+  });
+  it(' return 10025', () => {
+    expect(keepNumbers('100.25')).toEqual('10025');
   });
   it('undefined', () => {
     expect(getInitials(undefined)).toBeUndefined();
