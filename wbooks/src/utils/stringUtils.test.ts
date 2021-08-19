4488144744318c23eb2d5d37da95018568586a02
dezbrid <dezbrid@gmail.com>
2021-08-19 14:27:45 -0500

    fixed pr

--- wbooks/src/utils/stringUtils.test.ts
+++ wbooks/src/utils/stringUtils.test.ts
@@ -1,8 +1,17 @@
 import { getInitials, keepNumbers } from './stringUtils';
 
 describe('get initial latter', () => {
-  it('get W in Wolox', () => {
+  it('return W in Wolox', () => {
     expect(getInitials('wolox')).toEqual('w');
+  });
+  it('return `` ', () => {
+    expect(getInitials('')).toEqual('');
+  });
+  it('return `t i w` in `training in wolox`', () => {
+    expect(getInitials('training in wolox')).toEqual('t i w');
+  });
+  it('undefined', () => {
+    expect(getInitials(undefined)).toBeUndefined();
   });
 });
 
@@ -10,7 +19,16 @@ describe('keep only number', () => {
   it('get 2021 ', () => {
     expect(keepNumbers('wolox2021')).toEqual('2021');
   });
+  it('get 2021 ', () => {
+    expect(keepNumbers('wolox 2021')).toEqual('2021');
+  });
+  it('get 2021 ', () => {
+    expect(keepNumbers('19 de agosto 2021')).toEqual('192021');
+  });
   it('2021', () => {
     expect(keepNumbers(2021)).toEqual('2021');
+  });
+  it('undefined', () => {
+    expect(getInitials(undefined)).toBeUndefined();
   });
 });
