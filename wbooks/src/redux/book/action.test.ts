dbc0ba08960652f1539416f87759b88ee51ca697
dezbrid <dezbrid@gmail.com>
2021-08-23 10:28:05 -0500

    action filterbooks

---
+++ wbooks/src/redux/book/action.test.ts
@@ -0,0 +1,32 @@
+import configureStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
+import { BOOKS_MOCK } from '@constants/mockBooks';
+
+import Actions, { actions, target } from './action';
+
+const middlewares = [thunk];
+const mockStore = configureStore(middlewares);
+const initialState = { book: { books: [], filterSearch: '' } };
+const mockedStore = mockStore(initialState);
+beforeEach(() => mockedStore.clearActions());
+describe('actions filterBooks', () => {
+  mockedStore.dispatch(Actions.filterBooks('action'));
+  test('get filter', () => {
+    expect(mockedStore.getActions()).toEqual({
+      target: target.FILTER_SEARCH,
+      type: actions.FILTER_SEARCH,
+      payload: 'action'
+    });
+  });
+});
+
+describe('actions get books', () => {
+  mockedStore.dispatch(Actions.getBooks());
+  // api.get = jest.fn().mockImplementation(() => ({ ok: false, problem }));
+  test('success', () => {
+    expect(mockedStore.getActions()).toEqual([
+      { target: 'books', type: actions.BOOK_ACTION },
+      { target: 'books', type: actions.BOOK_ACTION_SUCCESS, payload: BOOKS_MOCK }
+    ]);
+  });
+});
--- wbooks/src/redux/book/reducer.test.ts
+++ wbooks/src/redux/book/reducer.test.ts
@@ -3,6 +3,8 @@ import { BOOKS_MOCK } from '@constants/mockBooks';
 import { actions } from './action';
 import booksReducer from './reducer';
 
+const ERROR = 'Not Found 404';
+
 const testActions = {
   getBooks: {
     type: actions.GET_BOOKS,
@@ -16,29 +18,29 @@ const testActions = {
   getBooksFailure: {
     type: actions.GET_BOOKS_FAILURE,
     target: 'books',
-    payload: 'Not Found 404'
+    payload: ERROR
   }
 };
 
 describe('@@BOOKS/GET_BOOKS', () => {
-  test('GET_BOOKS', () => {
+  it('GET_BOOKS', () => {
     expect(booksReducer(undefined, testActions.getBooks)).toMatchObject({
       books: [],
       booksError: null,
       booksLoading: true
     });
   });
-  test('GET_BOOKS_SUCCESS', () => {
+  it('GET_BOOKS_SUCCESS', () => {
     expect(booksReducer(undefined, testActions.getBooksSuccess)).toMatchObject({
       books: BOOKS_MOCK,
       booksError: null,
       booksLoading: false
     });
   });
-  test('GET_BOOKS_FAILURE', () => {
+  it('GET_BOOKS_FAILURE', () => {
     expect(booksReducer(undefined, testActions.getBooksFailure)).toMatchObject({
       books: [],
-      booksError: 'Not Found 404',
+      booksError: ERROR,
       booksLoading: false
     });
   });
