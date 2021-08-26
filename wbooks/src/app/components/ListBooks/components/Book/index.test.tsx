ca8b4597d3b90d8fd78bfe3721ce3a1d44413e6a
dezbrid <dezbrid@gmail.com>
2021-08-26 09:50:47 -0500

    test book

--- wbooks/src/app/components/ListBooks/components/Book/index.test.tsx
+++ wbooks/src/app/components/ListBooks/components/Book/index.test.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { fireEvent, render, waitFor } from '@testing-library/react-native';
+import { render } from '@testing-library/react-native';
 import { BOOKS_MOCK } from '@constants/mockBooks';
 
 import Book from './index';
@@ -18,19 +18,14 @@ jest.mock('@react-navigation/native', () => {
 
 describe('Testing Book', () => {
   test(' charged correctly data', () => {
-    const { queryByText } = render(<Book book={BOOKS_MOCK[0]} />);
+    const { queryByText, getByRole } = render(<Book book={BOOKS_MOCK[0]} />);
     const title = queryByText(BOOKS_MOCK[0].title);
     const author = queryByText(BOOKS_MOCK[0].author);
+    const touch = getByRole('button');
+    expect(touch).toBeTruthy();
     expect(title).toBeTruthy();
     expect(title?.props.children).toBe(BOOKS_MOCK[0].title);
     expect(author).toBeTruthy();
     expect(author?.props.children).toBe(BOOKS_MOCK[0].author);
-  });
-  test(' navigate to detail', async () => {
-    const { getByRole, findByText } = render(<Book book={BOOKS_MOCK[0]} />);
-    const touch = getByRole('button');
-    expect(touch).toBeTruthy();
-    fireEvent.press(touch);
-    await waitFor(() => findByText('BOOK DETAIL'));
   });
 });
